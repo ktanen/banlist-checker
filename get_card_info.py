4356895db689c6400d505cf903ccde8270609534
ktanen <kevinbtanen@gmail.com>
2026-03-25 09:28:50 -0400

    Fixed 'Not available' text because it's not necessarily true

--- get_card_info.py
+++ get_card_info.py
@@ -24,17 +24,17 @@ def get_card_info(card_name, game_format):
                 if "ban_tcg" in card_data["banlist_info"]:
                     banlist_status = card_data["banlist_info"]["ban_tcg"]
                 else:
-                    banlist_status = "not available"
+                    banlist_status = "not banned or not available"
             case GameFormat.ocg:
                 if "ban_ocg" in card_data["banlist_info"]:
                     banlist_status = card_data["banlist_info"]["ban_ocg"]
                 else:
-                    banlist_status = "not available"
+                    banlist_status = "not banned or not available"
             case GameFormat.goat:
                 if "ban_goat" in card_data["banlist_info"]:
                     banlist_status = card_data["banlist_info"]["ban_goat"]
                 else:
-                    banlist_status = "not available"
+                    banlist_status = "not banned or not available"
 
     
     result =  f"{card_name} is {banlist_status} in the {game_format.value} format."
